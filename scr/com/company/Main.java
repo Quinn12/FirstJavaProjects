0eeb318941304e3a475b947b24e8321f1d4fc8ca
Quinn12 <qhastings@students.mchenry.edu>
2018-09-01 01:01:33 -0500

    Add files via upload

---
+++ scr/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    System.out.println("Hi Im Quinn.");
+        System.out.println("Im 20.");
+        System.out.println("Im taking 3 classes.");
+        System.out.println("I work 5 day a week.");
+        System.out.println("I enjoy game design");
+    }
+}
